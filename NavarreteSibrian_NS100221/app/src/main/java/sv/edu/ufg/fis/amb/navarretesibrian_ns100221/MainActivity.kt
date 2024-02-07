5b872d30c3ea8b04732dc677f030fe2a0b750d99
VLADYY17 <ia.dennysnavarrete7@ufg.edu.sv>
2024-02-06 20:10:07 -0600

    segunda actividad vlady

---
+++ NavarreteSibrian_NS100221/app/src/main/java/sv/edu/ufg/fis/amb/navarretesibrian_ns100221/MainActivity.kt
@@ -0,0 +1,11 @@
+package sv.edu.ufg.fis.amb.navarretesibrian_ns100221
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ NavarreteSibrian_NS100221/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "NavarreteSibrian_NS100221"
+include(":app")
